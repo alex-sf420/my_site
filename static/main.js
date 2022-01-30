d9f251f8328910c71547899a882b898315f1923e
Alex <safronovalexx.87@gmail.com>
2022-01-30 15:12:12 +0300

    Настроено отображение двух графиков на странице

--- static/main.js
+++ static/main.js
@@ -1,10 +1,30 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+const ctx1 = document.getElementById('myChart1').getContext('2d');
+const myChart1 = new Chart(ctx1, {
     type: 'line',
     data: {
-        labels: [time],
+        labels: time,
         datasets: [{
-            label: '# of Votes',
+            label: 'Текущая загрузка',
+            data: values,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        responsive : false
+    }
+});
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: time,
+        datasets: [{
+            label: 'Среднее значение за 1 мин',
             data: values,
         }]
     },
