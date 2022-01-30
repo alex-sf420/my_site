c15bf04f185f37794c940e468e465fa9fb54cd1f
Alex <safronovalexx.87@gmail.com>
2022-01-30 22:30:49 +0300

    Доработано отображение диаграмм

--- static/main.js
+++ static/main.js
@@ -1,15 +1,26 @@
+Chart.defaults.color = "black";
+Chart.defaults.backgroundColor = "blue"
+
+
 const ctx1 = document.getElementById('myChart1').getContext('2d');
 const myChart1 = new Chart(ctx1, {
     type: 'line',
     data: {
         labels: time,
+        borderColor: 'pink',
         datasets: [{
             label: 'Текущая загрузка',
             data: value,
+            scaleFontColor: "#FFFFFF"
         }]
     },
     options: {
         scales: {
+            xAxis: {
+                ticks: {
+                    maxTicksLimit: 10
+                }
+            },
             y: {
                 beginAtZero: true
             }
@@ -30,6 +41,11 @@ const myChart2 = new Chart(ctx2, {
     },
     options: {
         scales: {
+            xAxis: {
+                ticks: {
+                    maxTicksLimit: 10
+                }
+            },
             y: {
                 beginAtZero: true
             }
@@ -37,3 +53,5 @@ const myChart2 = new Chart(ctx2, {
         responsive : false
     }
 });
+
+ 
