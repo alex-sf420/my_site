f734de994ef973432d313e7ee9312417475084f1
Alex <safronovalexx.87@gmail.com>
2022-01-30 21:28:12 +0300

    Рефакторинг кода. Повторяющийся код вынесен в отдельные функции. Добавлены комментарии

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 import  json
 from statistics import mean
 
-INTERVAL = 1 # константа, определяющая интервал записи в БД в сек.
+INTERVAL = 5 # константа, определяющая интервал записи в БД в сек.
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/blog.db'
@@ -21,9 +21,18 @@ class Cpuusage(db.Model):
     value = db.Column(db.Float)                                 # загрузка ЦП
     date = db.Column(db.DateTime, default=datetime.utcnow())    # дата/время внесения в БД
 
-# def
+def make_empty_lists(info):
+    """
+    Принамает список базы данных. Возвращает список пустых списков,
+    соответствующих количеству минут
+    """
+    return [[] for x in range(len(info) // (60//INTERVAL))] # по количеству минут
 
-
+def calculates_the_average_value(array):
+    """
+    Принимает двумерный список. Возвращает список средних значений
+    """
+    return [round(mean(x), 1) for x in array]
 
 @app.route('/')
 def index():
@@ -34,21 +43,29 @@ def index():
     Отображает страницу html с переданной ей информацией о загрузке ЦП
     """
     now = datetime.now()
+    # получаем значения базы данных по столбцам value и date
+    # за указанный период
     info = db.session.query(Cpuusage.value, Cpuusage.date).filter(Cpuusage.date >\
                                                                   (now - timedelta(hours=1))).all()
+    # формируем отдельные списки данных для первого графика
     value = [x[0] for x in info]
     date = [x[1] for x in info]
-    average_value = [[] for x in range(len(info)// (5//INTERVAL))] # по количеству минут
-    average_date = [[] for x in range(len(info)// (5//INTERVAL))]
-    counter = 0
-    for i in range(len(info) // (5//INTERVAL)): # количество циклов по количеству минут
-        for j in range(5 // INTERVAL):
+    # создаем пустые списки для второго графика
+    average_value = make_empty_lists(info)
+    average_date = make_empty_lists(info)
+    # заполняем сведения для второго графика двумерными списками
+    counter = 0 # счетчик для сплошного обхода списков
+    for i in range(len(info) // (60//INTERVAL)): # количество циклов по количеству минут
+        for j in range(60 // INTERVAL): # количество записей в БД в минуту
             average_value[i].append(value[counter])
+            # переводим в Unix time, умножая на 1000 для корректного чтения в js
             average_date[i].append(time.mktime(date[counter].timetuple()) * 1000)
             counter += 1
     else:
-        average_value = [round(mean(x), 1) for x in average_value]
-        average_time_for_js = [round(mean(x), 1) for x in average_date]
+        # считаем среднее значение за 1 мин для второго графика
+        average_value = calculates_the_average_value(average_value)
+        average_time_for_js = calculates_the_average_value(average_date)
+    # переводим в Unix time список для первого графика
     time_for_js = [time.mktime(date[x].timetuple()) * 1000 for x in range(len(date))]
     return render_template('index.html', values=json.dumps(value), time=json.dumps(time_for_js),\
                            average_value=json.dumps(average_value), \
